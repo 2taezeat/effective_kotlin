4fbbf61f867b5ee9ca9a5c4546baf48dc325cf2a
2taezeat <xorudfl772@naver.com>
2023-05-27 22:26:19 +0900

    부록, 용어 정리. Appendix.md

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -7,5 +7,6 @@ fun main(args: Array<String>) {
     // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
     println("Program arguments: ${args.joinToString()}")
     val a = listOf<Int>(1,2,3)
-
+    val b = mutableListOf(1,2,3)
+    b.add(4)
 }
